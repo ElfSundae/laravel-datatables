20448ec10577519e8550a7f66279961c407dacf2
Elf Sundae <elf.sundae@gmail.com>
2017-11-26 18:24:31 +0800

    Add DataTables tests

---
+++ tests/DataTablesTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ElfSundae\Laravel\DataTables\Test;
+
+use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Collection;
+use Yajra\DataTables\CollectionDataTable;
+use ElfSundae\Laravel\DataTables\DataTablesServiceProvider;
+
+class DataTablesTest extends TestCase
+{
+    public function testMakeDataTableForArray()
+    {
+        $this->assertInstanceOf(CollectionDataTable::class, datatables([]));
+    }
+
+    public function testMakeDataTableUsingConfiguredClassName()
+    {
+        $this->app['config']->set([
+            'datatables.builders.'.CustomCollection::class => CustomCollectionDataTable::class,
+        ]);
+
+        $this->assertInstanceOf(
+            CustomCollectionDataTable::class,
+            datatables(new CustomCollection)
+        );
+    }
+
+    public function testMakeDataTableUsingConfiguredEngineAlias()
+    {
+        $this->app['config']->set([
+            'datatables.builders.'.CustomCollection::class => 'custom_collection',
+            'datatables.engines.custom_collection' => CustomCollectionDataTable::class,
+        ]);
+
+        $data = new CustomCollection([]);
+
+        $this->assertInstanceOf(
+            CustomCollectionDataTable::class,
+            datatables($data)
+        );
+
+        $this->assertInstanceOf(
+            CustomCollectionDataTable::class,
+            datatables()->customCollection($data)
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [DataTablesServiceProvider::class];
+    }
+}
+
+class CustomCollectionDataTable extends CollectionDataTable
+{
+}
+
+class CustomCollection extends Collection
+{
+}
