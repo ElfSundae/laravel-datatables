2ab8c1b524bc2525bf9c1d2f76eebbe7350b4e96
Elf Sundae <elf.sundae@gmail.com>
2017-11-27 09:07:06 +0800

    Revert "Remove custom Html\Builder"
    This reverts commit a18619c58467375afd0f20b67ff515b9e3f38e2e.

--- src/DataTablesServiceProvider.php
+++ src/DataTablesServiceProvider.php
@@ -45,6 +45,8 @@ class DataTablesServiceProvider extends ServiceProvider
         $this->app->singleton('datatables', function () {
             return new DataTables;
         });
+
+        $this->app->bind('datatables.html', Html\Builder::class);
     }
 
     /**
---
+++ src/Html/Builder.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ElfSundae\Laravel\DataTables\Html;
+
+use Yajra\DataTables\Html\Builder as BaseBuilder;
+
+class Builder extends BaseBuilder
+{
+}
