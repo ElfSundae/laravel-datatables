2335cfeacf0452f9f310f663493cb0d3700dc3db
Elf Sundae <elf.sundae@gmail.com>
2017-11-28 00:56:57 +0800

    Add postAjax() to Html Builder

--- src/Html/Builder.php
+++ src/Html/Builder.php
@@ -2,8 +2,30 @@
 
 namespace ElfSundae\Laravel\DataTables\Html;
 
+use Illuminate\Support\Arr;
 use Yajra\DataTables\Html\Builder as BaseBuilder;
 
 class Builder extends BaseBuilder
 {
+    /**
+     * Setup "ajax" parameter with POST method.
+     *
+     * @param  string|array  $attributes
+     * @return $this
+     */
+    public function postAjax($attributes = [])
+    {
+        if (! is_array($attributes)) {
+            $attributes = ['url' => (string) $attributes];
+        }
+
+        if (app()->bound('session') && $token = app('session')->token()) {
+            $attributes = Arr::add($attributes, 'headers.X-CSRF-TOKEN', $token);
+        }
+
+        return $this->ajax(array_merge(
+            Arr::except($attributes, 'method'),
+            ['type' => 'POST']
+        ));
+    }
 }
