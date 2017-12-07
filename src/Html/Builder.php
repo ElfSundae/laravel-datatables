e9cca484e182eb8862ea8b663d20664a542e941a
Elf Sundae <elf.sundae@gmail.com>
2017-12-07 13:34:22 +0800

    Remove postAjax from Html Builder
    See https://github.com/yajra/laravel-datatables-html/pull/46

--- src/Html/Builder.php
+++ src/Html/Builder.php
@@ -7,25 +7,4 @@ use Yajra\DataTables\Html\Builder as BaseBuilder;
 
 class Builder extends BaseBuilder
 {
-    /**
-     * Setup "ajax" parameter with POST method.
-     *
-     * @param  string|array  $attributes
-     * @return $this
-     */
-    public function postAjax($attributes = [])
-    {
-        if (! is_array($attributes)) {
-            $attributes = ['url' => (string) $attributes];
-        }
-
-        if (app()->bound('session') && $token = app('session')->token()) {
-            $attributes = Arr::add($attributes, 'headers.X-CSRF-TOKEN', $token);
-        }
-
-        return $this->ajax(array_merge(
-            Arr::except($attributes, 'method'),
-            ['type' => 'POST']
-        ));
-    }
 }
